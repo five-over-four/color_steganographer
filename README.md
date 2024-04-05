18758d8db96cc483f31f6b160cfcd6e673309cc6
not-legato <tommirossidude@gmail.com>
2024-04-05 18:35:03 +0300

    complete rewrite, new features.

--- README.md
+++ README.md
@@ -1,41 +1,47 @@
 # Colour Steganographer
 This fun little tool will encode any message you write into the three
 colour channels of any image of your choosing. It will do this by altering the
-parity of that channels' colour codes, in order of red, green, blue. An even number corresponds with a 0 and an odd one with 1.
+remainder under modular arithmetic of that channels' colour codes, in order of red, green, blue.
 
-Each message begins and ends with an alternating sequence of 24 1s and 0s; 10101010...
+Encode your message into `image.png` with the command `python stegano.py image.png -t "this is my message."` or from a file `source.txt` by using `python stegano.py image.png -i source.txt`
 
-Encode your message into `image.png` with the command `python stegano.py image.png -e "this is my message."` or from a file `source.txt` by using `python stegano.py image.png -i source.txt`
-
-Decode such a message from `encoded.png` with the command `python stegano.py encoded.png -d`. If the encoded message is very long, it's recommended you pipe the result into a file with the `>` operator; `python stegano.py encoded.png -d > target.txt`.
+Decode such a message from `encoded.png` with the command `python stegano.py encoded.png -d`. If the encoded message is very long, it's recommended you pipe the result into a file with the `>` operator; `python stegano.py encoded.png -d > target.txt`. To use more bits of each colour for the message, use the `-b` flag, with numbers 1-8, 1 being the least bits (most discreet) and 8 being the most extreme (0 bits for colour information!).
 
 ## Help
-    usage: Simple Binary Steganography Tool [-h] [-i TEXTFILE] [-e MESSAGE] [-d] [-a] filename
+    usage: Simple Binary Steganography Tool [-h] [-i TEXTFILE] [-t MESSAGE] [-b BITS_PER_PIXEL] [-d] [-a] filename
 
     Encode and decode a message into and from the colour channels of an image.
 
     positional arguments:
-    filename              name of the image file.
+    filename              Name of the image file.
 
     options:
     -h, --help            show this help message and exit
     -i TEXTFILE, --input TEXTFILE
-                            encode the contents of a text file into the image.
-    -e MESSAGE, --encode MESSAGE
-                            encode a message into the image file.
-    -d, --decode          read a message from the image file.
-    -a, --analyze         gives storage constraints for the image.
+                            Encode the contents of a text file into the image.
+    -t MESSAGE, --typemessage MESSAGE
+                            Type directly to encode a message into the image file.
+    -b BITS_PER_PIXEL, --bitlevel BITS_PER_PIXEL
+                            Store n bits per pixel. Higher = less discreet, as the colours are represented in fewer bits.
+    -d, --decode          Read a message from the image file.
+    -a, --analyze         Gives storage constraints for the image.
+
+## Method of steganography used
+Each pixel in the image contains three colours, red, green, and blue. Their values range from 0 to 255 (2^8 values, 8 bits). By adjusting
+each colour value to the nearest value (random direction) that corresponds with a binary representation of your text data, we're able to
+encode bit_level bits of information in each colour channel of each pixel in mod 2^bit_level modular arithmetic.
+
+For example: at bit_level = 3, we're working with 2^3 = mod 8 arithmetic. 0 = "000", 1 = "001", ..., 6 = "110", and 7 = "111". This would
+take the least significant 3 bits from the colour channel to what is essentially random-ish noise and leave 5 to the actual colour data.
+This naturally introduces some noise, but is completely invisible at low bit_levels (up to about 4). At 8 bits, the entire underlying image is lost, as
+0 bits of information are retained of it.
 
 ## Space considerations
-As each pixel can contain 3 bits of information and the starting sequence takes up 24 bits (end sequence can be omitted), the maximum number of characters you can encode into an image of size width * height is `(width * height * 3)/8 - 24`, rounded down.
+As each pixel can contain 3 * bit_level bits of information and the starting sequence takes up 24 bits (end sequence can be omitted), the maximum number of characters you can encode into an image of size width * height is `(width * height * 3 * bit_level)/8 - 24`, rounded down.
 
-For instance, a 400 x 400 image can hold (400 * 400 * 3)/8 - 24 = 59976 characters, or about 60 kilobytes of information.
+For instance, at bit_level 1, a 400 x 400 image can hold (400 * 400 * 3)/8 - 24 = 59976 characters, or about 60 kilobytes of information.
 
-Note that compressing the image after encoding will likely destroy the encoded information.
+Note that compressing the image after encoding will likely destroy the encoded information as it relies on precise numerical values.
 
 ## Included example
-I've encoded something into the `example_encoded.png` that you can test the program on. Simply run
-
-`python stegano.py example_encoded.png -d > result.txt`
-
-to see what it is.
+I've encoded something into the `example_encoded.png` that you can test the program on. Simply run `python stegano.py example_encoded.png -d > result.txt` to see what it is.
--- stegano.py
+++ stegano.py
@@ -1,7 +1,7 @@
-from PIL import Image
 from random import choice
+from itertools import product
 import argparse
-# import base64
+from PIL import Image
 
 #   #   #   #   #   #   #   #   #   #   #   #   #   #   #    #
 # Define basic bin -> ascii and ascii -> bin functions here. #
@@ -14,14 +14,15 @@ def to_bin(s: str):
     """
     return "".join([ str(bin(ord(char)))[2:].zfill(8) for char in s])
 
-def to_ascii(b: str):
+def to_ascii_old(b: str):
     """
     Returns a string of characters from a string of bytes:
     "0110100001100101011011000110110001101111" => "hello"
     """
     pos = 0
     s = ""
-    while pos * 8 < len(b):
+    max_length = len(b)
+    while pos * 8 < max_length:
         s += decode_byte(b[pos * 8:pos * 8 + 8])
         pos += 1
     return s
@@ -33,98 +34,118 @@ def decode_byte(b: str) -> str:
     """
     return chr(int(bytes(b, "utf-8"), 2))
 
-#   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #
-# Actual image encodings and decodings here.                #
-#   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #
-
-channels = {"red": 0, "green": 1, "blue": 2}
-
-def encode_message(img, msg):
+def bit_combinations(power=1):
     """
-    writes an ascii message (msg) into the colour channels
-    of your chosen image as a sequence of 1s and 0s.
-    Sequence: first red, then green, then blue.
+    Generates ["0", "1"] with power==1
+    Generates ["00", "01", "10", "11"] with power==2
+    And so forth.
     """
-    write_binary_data(img, to_bin(msg))
+    combos = product(["0", "1"], repeat=power)
+    combos = ["".join(x) for x in combos]
+    return combos
 
-def decode_message(img):
-    """
-    Reads a potential message encoded by this program from
-    an image file (img), hidden in the colour channels.
-    A message begins with 24 0s and ends with 24 0s in a row.
-    """
-    return to_ascii(get_binary_data(img))
+def to_ascii(b):
+    pos = 0
+    s = ""
+    max_length = len(b)
+    while pos * 8 < max_length:
+        s += decode_byte(b[pos * 8:pos * 8 + 8])
+        pos += 1
+    return s
 
-def get_binary_data(img):
+# this is pretty slow for now, because it loops over all the pixels individually.
+# TODO: use numpy or build a little queue that checks for termination string.
+def decode_message(img, mod_power = 1):
     """
     Reads ch channels' parity as 0 or 1 into b and returns.
     Terminates on 24 bits of "10101010..."
+
+    mod_power means we encode 2**mod_power bits per pixel, resulting in
+    more aggressive rounding of values and thus more visible changes.
+    Each increase in mod_power doubles the storage capacity.
     """
     b = ""
     key_seq = "10"*12
-    for ch in ("red", "green", "blue"):
-        for x in range(width):
-            for y in range(height):
-                b += str(img[x, y][channels[ch]] % 2)
-                if width * y + x == 24 and b[:24] != key_seq:
+    bit_data = bit_combinations(mod_power)
+    steps = 0
+
+    for x in range(width):
+        for y in range(height):
+            for ch in ("red", "green", "blue"):
+                arity = img[x, y][channels[ch]] % (2**mod_power)
+                b += bit_data[arity]
+                steps += 1
+                if steps == 24 and b[:24] != key_seq:
+                    print(b)
                     return to_bin("no message found!")
     try:
-        endpoint = b[24:].index(key_seq) + 24
+        endpoint = b[24:].index(key_seq)
         return b[24: endpoint] # cut out everything outside key_seq
     except Exception: # the image was too small to contain the key_seq at the end.
         return b[24:]
+    
 
-def write_binary_data(img, msg):
+def encode_message(img, msg, mod_power=1):
     """
     Writes an already encoded string of bytes into the pixels of
     the chosen image from a given message. msg is a sequence
     of 1s and 0s.
+
+    mod_power writes mod_power bit(s) of information per pixel.
+    (until the message ends or the pixels end.)
     """
     # message ends and starts with 24 bits of alternating 1s and 0s.
-    msg = "10"*12 + msg + "10"*12
+    # 24 empty characters are added to the end, because it's possible that
+    # the 10101010... sequence is PRECEDED by a pattern that extends the sequence,
+    # therefore trashing the characters.
+    msg = "10"*12 + to_bin(msg) + "0"*24 + "10"*12
+    bit_data = bit_combinations(mod_power)
+
+    # pad to make msg length divisible by mod_power, so it'll process nicely with
+    # msg_segment down in the next block.
+    msg_length = len(msg)
+    if msg_length % mod_power != 0:
+        msg += (mod_power - (msg_length % mod_power)) * "0"
     msg_pos = 0
-    for ch in ("red", "green", "blue"): # start with red, fill other channels as needed.
-        for x in range(width):
-            for y in range(height):
+    
+    for x in range(width):
+        for y in range(height):
+            for ch in ("red", "green", "blue"):
                 if msg_pos >= len(msg):
                     return 1
-                if msg[msg_pos] == "0":
-                    img[x, y] = replace_pixel(img[x, y], round_even(img[x, y][channels[ch]]), ch)
-                else:
-                    img[x, y] = replace_pixel(img[x, y], round_odd(img[x, y][channels[ch]]), ch)
-                msg_pos += 1
+                msg_segment = msg[msg_pos:msg_pos + mod_power] # this many bits [010]0101011101 with mod_power==3.
+                img[x, y] = generate_colour_tuple(img[x, y],
+                                            round_to_congruence(img[x, y][channels[ch]],
+                                                         bit_data.index(msg_segment), 
+                                                         2**mod_power), ch)
+                msg_pos += mod_power
 
-def round_even(n):
+def round_to_congruence(k, end_remainder, modulus=2):
     """
-    Rounds an integer to a near even number.
+    Rounds an integer to the nearest integer n such that n % modulus == end_remainder.
     """
-    if n == 255:
-        return 254
-    return n if (n % 2 == 0) else n + choice([-1,1])
+    remainder = k % modulus
+    end_remainder %= modulus # make sure it's not modulus itself.
+    rem_diff = end_remainder - remainder
+    n = k + rem_diff
+    if n > k and n - modulus >= 0: return choice([n, n - modulus])
+    elif n < k and n + modulus <= 255: return choice([n, n + modulus])
+    elif n < 0: return n + modulus
+    elif n > 255: return n - modulus
+    return n
 
-def round_odd(n):
-    """
-    Rounds an integer to near odd number.
-    """
-    if n == 0:
-        return 1
-    return n if (n % 2 == 1) else n + choice([-1,1])
-
-# this is really clumsy and slow.
-def replace_pixel(pixel, new_val, ch):
+def generate_colour_tuple(pixel, new_val, ch):
     """
     Returns a pixel 3-tuple (r,g,b), where the ch-corresponding
     colour is replaced by new_val.
     """
     r, g, b = pixel
-    if ch == "red":
-        return (new_val, g, b)
-    elif ch == "green":
-        return (r, new_val, b)
-    else:
-        return (r, g, new_val)
+    match ch:
+        case "red": return (new_val, g, b)
+        case "green": return (r, new_val, b)
+        case "blue": return (r, g, new_val)
     
-def analyze_file(img):
+def analyze_file():
     """
     Gives some heuristic constraints for how much data can be encoded
     within the image file.
@@ -138,10 +159,13 @@ def analyze_file(img):
     def round_proper(val):
         return Decimal(str(val)).quantize(Decimal('.01'), rounding=ROUND_UP)
     
-    return f"The image resolution is {width} x {height}.\n" \
-            f"Maximum stored characters: {max_size} or {round_proper(max_size/1000)}kB\n" \
-            f"Roughly {floor(max_size/6)} words or {round_proper(max_size/(6*280))} pages for a book with 280 words per page."
-
+    def storage_estimates():
+        levelinfo = ""
+        for blevel in range(1,9):
+            levelinfo += f"{blevel}: {max_size * blevel} / {round_proper(max_size/1000 * blevel)}kB\n"
+        return f"Storage capacity at each bit level:\n{levelinfo}"
+    
+    return storage_estimates()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -149,26 +173,39 @@ if __name__ == "__main__":
                     description="Encode and decode a message into and from the colour channels\nof an image.",
                     epilog="")
     
-    parser.add_argument("filename", help="name of the image file.")
-    parser.add_argument("-i", "--input", metavar="TEXTFILE", help="encode the contents of a text file into the image.")
-    parser.add_argument("-e", "--encode", metavar="MESSAGE", nargs=1, help="encode a message into the image file.")
-    parser.add_argument("-d", "--decode", action="store_true", help="read a message from the image file.")
-    parser.add_argument("-a", "--analyze", action="store_true", help="gives storage constraints for the image.")
-    args = parser.parse_args()
+    parser.add_argument("filename", help="Name of the image file.")
+    parser.add_argument("-i", "--input", metavar="TEXTFILE", help="Encode the contents of a text file into the image.")
+    parser.add_argument("-t", "--typemessage", metavar="MESSAGE", help="Type directly to encode a message into the image file.")
+    parser.add_argument("-b", "--bitlevel", metavar="BITS_PER_PIXEL", help="Store n bits per pixel. Higher = less discreet, as the colours are represented in fewer bits.")
+    parser.add_argument("-d", "--decode", action="store_true", help="Read a message from the image file.")
+    parser.add_argument("-a", "--analyze", action="store_true", help="Gives storage constraints for the image.")
+    argv = parser.parse_args()
     
-    image = Image.open(args.filename).convert("RGB")
-    img = image.load()
-    width, height = image.size
+    # global vars; bit_level controls the modular arithmetic base and thus the
+    # number of bits of info you get per pixel. +1bit = x2 storage (and noise!)
+    try:
+        image = Image.open(argv.filename).convert("RGB")
+        img = image.load()
+        width, height = image.size
+        channels = {"red": 0, "green": 1, "blue": 2}
+        bit_level = int(argv.bitlevel) if argv.bitlevel else 1
+        bit_level = bit_level if (0 < bit_level <= 8) else 1
+    except FileNotFoundError:
+        print("No such file found.")
+        argv = None # dirty hack, vol. 1
 
-    if args.decode:
-        print(decode_message(img))
-    elif args.input:
-        text = open(args.input, "r").read()
+    if not argv: # dirty hack, the finale.
+        pass
+    elif argv.decode:
+        print(to_ascii(decode_message(img, bit_level)))
+    elif argv.input:
+        text = open(argv.input, "r").read()
         stripped = "".join((c for c in text if 0 < ord(c) < 255)) # stupid unicode.
-        encode_message(img, stripped)
+        encode_message(img, stripped, bit_level)
         image.save("encoded.png")
-    elif args.encode:
-        encode_message(img, args.encode)
+        print(f"Encoded with bit_level = {bit_level}")
+    elif argv.typemessage:
+        encode_message(img, argv.typemessage, bit_level)
         image.save("encoded.png")
-    elif args.analyze:
-        print(analyze_file(img))
+    elif argv.analyze:
+        print(analyze_file())
